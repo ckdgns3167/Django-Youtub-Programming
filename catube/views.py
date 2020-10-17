f155e3bb1b86a5f983f1040733016786af2b4c53
newckdgns <ckdgns3167@gmail.com>
2020-10-17 16:10:18 +0900

    클래스 기반 뷰로 video_list 뷰 구현

--- catube/views.py
+++ catube/views.py
@@ -1,10 +1,13 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 from .models import Video
 
+# def video_list(request):
+#     qs = Video.objects.all()  # QuerySet 객체
+#     # catube/templates/catube/video_list.html
+#     return render(request, 'catube/video_list.html', {
+#         'video_list': qs,
+#     })
+# 함수를 만들어준다.
 
-def video_list(request):
-    qs = Video.objects.all()  # QuerySet 객체
-    # catube/templates/catube/video_list.html
-    return render(request, 'catube/video_list.html', {
-        'video_list': qs,
-    })
+video_list = ListView.as_view(model=Video, paginate_by=2)
