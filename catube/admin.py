6a31ef4389444e0a984bd8ed27a1450db3bcc866
newckdgns <ckdgns3167@gmail.com>
2020-10-17 15:30:32 +0900

    파일 유효성 검사 로직 추가

--- catube/admin.py
+++ catube/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Video
+from .forms import VideoForm
 
-admin.site.register(Video)
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at']
+    search_fields = ['title']
+    form = VideoForm
+
+
+admin.site.register(Video, VideoAdmin)
