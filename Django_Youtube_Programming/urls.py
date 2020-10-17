50fbca2c34da173785c09f90ad69113d531d5939
newckdgns <ckdgns3167@gmail.com>
2020-10-17 15:40:38 +0900

    함수 기반 뷰로 video_list 뷰 구현

--- Django_Youtube_Programming/urls.py
+++ Django_Youtube_Programming/urls.py
@@ -2,7 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from catube import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.video_list),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catube/views.py
+++ catube/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Video
 
-# Create your views here.
+
+def video_list(request):
+    qs = Video.objects.all()  # QuerySet 객체
+    # catube/templates/catube/video_list.html
+    return render(request, 'catube/video_list.html', {
+        'video_list': qs,
+    })
